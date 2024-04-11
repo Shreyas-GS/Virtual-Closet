f169660edca42de04f416e310fa35ac6676de567
Shreyas <shreyasgsm25@gmail.com>
2024-04-11 18:54:30 +0530

    Frontend done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
+import Closet from "./components/Closet";
+import UploadForm from "./components/UploadForm";
 
-function App() {
+const App = () => {
+  const [items, setItems] = useState([]);
+
+  const handleUpload = (formData) => {
+    // Implement logic to upload item to server and update state
+    console.log(formData);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <UploadForm onUpload={handleUpload} />
+      <Closet items={items} />
     </div>
   );
-}
+};
 
 export default App;
